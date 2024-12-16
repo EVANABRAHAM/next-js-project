740b24cdbc5add737219905b400d15429edd2c05
EVANABRAHAM <evantabraham842@gmail.com>
2024-12-17 01:37:43 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  console.log("what i am doing here  -- server/clinet");
+  return (
+    <h1 className="text-3xl">Welcome to Next.js</h1>
+  );
+}
